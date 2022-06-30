3e31b0acf998598e387f96d87de0820a17d032f3
ReantoAlvaL <renato.alval@gmail.com>
2022-06-30 02:41:48 -0400

    Ventas Commit

--- controllers/crud.js
+++ controllers/crud.js
@@ -1,6 +1,6 @@
 const connection = require('../database/db');
 
-//Guardar registro
+//Guardar registro CLIENTE
 exports.save  = (req, res) => {
     const nombre_cli = req.body.nombre_cli;
     const deuda_cli = req.body.deuda_cli;
@@ -14,7 +14,7 @@ exports.save  = (req, res) => {
     })
 }
 
-//Editar registro
+//Editar registro CLIENTE
 exports.update = (req, res) => {
     const id_cli = req.body.id_cli;
     const nombre_cli = req.body.nombre_cli;
@@ -30,12 +30,71 @@ exports.update = (req, res) => {
 }
 
 
+//Guardar registro VENTAS
+exports.save_ventas  = (req, res) => {
+    const producto_ventas = req.body.producto_ventas;
+    const tipo_ventas = req.body.tipo_ventas;
+    const cantidad_ventas = req.body.cantidad_ventas;
+    const valor_ventas = req.body.valor_ventas;
+    connection.query('INSERT INTO ventas SET ?', {producto_ventas:producto_ventas, tipo_ventas:tipo_ventas, cantidad_ventas:cantidad_ventas, valor_ventas:valor_ventas}, (error, results)=>{
+        if(error){
+            console.log(error);
+        }else{
+            res.redirect ('ventas');
+        }
+    })
+}
 
 
+//Editar registro VENTAS
+exports.update_ventas = (req, res) => {
+    const id_ventas = req.body.id_ventas;
+    const producto_ventas = req.body.producto_ventas;
+    const tipo_ventas = req.body.tipo_ventas;
+    const cantidad_ventas = req.body.cantidad_ventas;
+    const valor_ventas = req.body.valor_ventas;
+    connection.query('UPDATE ventas SET ? WHERE id_ventas = ?',[{producto_ventas:producto_ventas, tipo_ventas:tipo_ventas, cantidad_ventas:cantidad_ventas, valor_ventas:valor_ventas}, id_ventas], (error, results)=>{
+        if(error){
+            console.log(error);
+        }else{           
+            res.redirect('ventas');         
+        }
+    })
+}
+
+//Guardar registro INVENTARIO
+exports.save_inventario  = (req, res) => {
+    const nombre_inv = req.body.nombre_inv;
+    const tipo_inv = req.body.tipo_inv;
+    const cantidad_inv = req.body.cantidad_inv;
+    const valor_inv = req.body.valor_inv;
+    connection.query('INSERT INTO inventario SET ?', {nombre_inv:nombre_inv, tipo_inv:tipo_inv, cantidad_inv:cantidad_inv, valor_inv:valor_inv}, (error, results)=>{
+        if(error){
+            console.log(error);
+        }else{
+            res.redirect ('ventas');
+            res.redirect ('inventario');
+        }
+    })
+}
 
 
-
-
+//Editar registro INVENTARIO
+exports.update_inventario = (req, res) => {
+    const id_inv = req.body.id_inv;
+    const nombre_inv = req.body.nombre_inv;
+    const tipo_inv = req.body.tipo_inv;
+    const cantidad_inv = req.body.cantidad_inv;
+    const valor_inv = req.body.valor_inv;
+    connection.query('UPDATE inventario SET ? WHERE id_inv = ?',[{nombre_inv:nombre_inv, tipo_inv:tipo_inv, cantidad_inv:cantidad_inv, valor_inv:valor_inv}, id_inv], (error, results)=>{
+        if(error){
+            console.log(error);
+        }else{           
+            res.redirect('ventas');   
+            res.redirect ('inventario');      
+        }
+    })
+}
 
 
 
--- router.js
+++ router.js
@@ -3,15 +3,6 @@ const router = express.Router();
 
 const conexion = require('./database/db');
 
-router.get('/clientes', (req, res)=>{     
-    conexion.query('SELECT * FROM clientes',(error, results)=>{
-        if(error){
-            throw error;
-        } else {                       
-            res.render('clientes.ejs', {results:results});            
-        }   
-    })
-})
 
 router.get('/proveedores', (req, res)=>{     
     conexion.query('SELECT * FROM proveedores',(error, results)=>{
@@ -33,15 +24,7 @@ router.get('/informes', (req, res)=>{
     })
 })
 
-router.get('/inventario', (req, res)=>{     
-    conexion.query('SELECT * FROM inventario',(error, results)=>{
-        if(error){
-            throw error;
-        } else {                       
-            res.render('inventario.ejs', {results:results});            
-        }   
-    })
-})
+
 
 router.get('/pedidos', (req, res)=>{     
     conexion.query('SELECT * FROM pedidos',(error, results)=>{
@@ -53,15 +36,7 @@ router.get('/pedidos', (req, res)=>{
     })
 })
 
-router.get('/ventas', (req, res)=>{     
-    conexion.query('SELECT * FROM ventas',(error, results)=>{
-        if(error){
-            throw error;
-        } else {                       
-            res.render('ventas.ejs', {results:results});            
-        }   
-    })
-})
+
 
 router.get('/info_generada', (req, res)=>{     
     conexion.query('SELECT * FROM clientes',(error, results)=>{
@@ -73,6 +48,8 @@ router.get('/info_generada', (req, res)=>{
     })
 })
 
+
+//---------------------------> CRUD CLIENTES <--------------------------------
 
 router.get('/create_cli', (req, res) => {
     conexion.query('SELECT * FROM clientes',(error, results)=>{
@@ -106,10 +83,91 @@ router.get('/delete/:id_cli', (req, res) => {
     });
 });
 
+
+
+
+//---------------------------> CRUD VENTAS <--------------------------------
+
+router.get('/ventas', (req, res) => {
+    conexion.query('SELECT * FROM ventas',(error, results)=>{
+        if(error){
+            throw error;
+        } else {                       
+            res.render('ventas.ejs', {results:results});            
+        }   
+    })
+})
+
+router.get('/ventas/:id_ventas', (req,res)=>{    
+    const id_ventas = req.params.id_ventas;
+    connection.query('SELECT * FROM ventas WHERE id_ventas=?',[id_ventas] , (error, results) => {
+        if (error) {
+            throw error;
+        }else{            
+            res.render('ventas', {ventas:results[0]});            
+        }        
+    });
+});
+
+router.get('/delete_ventas/:id_ventas', (req, res) => {
+    const id_ventas = req.params.id_ventas;
+    connection.query('DELETE FROM ventas',[id_ventas], (error, results)=>{
+        if(error){
+            console.log(error);
+        }else{           
+            res.redirect('/');             
+        }
+    });
+});
+
+
+//---------------------------> CRUD INVENTARIO <--------------------------------
+
+router.get('/inventario', (req, res)=>{     
+    conexion.query('SELECT * FROM inventario',(error, results)=>{
+        if(error){
+            throw error;
+        } else {                       
+            res.render('inventario.ejs', {results:results});          
+        }   
+    })
+})
+
+
+
+router.get('/inventario/:id_inv', (req,res)=>{    
+    const id_inv = req.params.id_inv;
+    connection.query('SELECT * FROM inventario WHERE id_inv=?',[id_inv] , (error, results) => {
+        if (error) {
+            throw error;
+        }else{            
+            res.render('ventas', {ventas:results[0]});            
+        }        
+    });
+});
+
+router.get('/delete_inventario/:id_inventario', (req, res) => {
+    const id_inv = req.params.id_inv;
+    connection.query('DELETE FROM inventario WHERE id_inv=?',[id_inv], (error, results)=>{
+        if(error){
+            console.log(error);
+        }else{           
+            res.redirect('/');             
+        }
+    });
+});
+
+
+
+
 const crud = require('./controllers/crud');
 const connection = require('./database/db');
+
 router.post('/save', crud.save);
 router.post('/update', crud.update);
+
+router.post('/save_ventas', crud.save_ventas);
+router.post('/update_ventas', crud.update_ventas);
 
 module.exports = router;
 
